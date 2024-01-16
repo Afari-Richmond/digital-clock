a8cf64e512919f715c529f295996e893fc77e596
Afari Richmond <afaririchmond1000@gmail.com>
2024-01-16 13:18:00 +0000

    Created the feature to change the time every one second

---
+++ script.js
@@ -0,0 +1,16 @@
+let hours = document.getElementById("hrs");
+let minutes = document.getElementById("min");
+let seconds = document.getElementById("sec");
+
+
+setInterval(()=>{
+
+    let currentTime = new Date();
+    hours.innerHTML = currentTime.getHours();
+    minutes.innerHTML = currentTime.getMinutes();
+    seconds.innerHTML = currentTime.getSeconds();
+    
+}, 1000)
+
+
+console.log(currentTime);
